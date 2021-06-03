199d4ab30e569d94560a0dd37a6c27d7120cd133
dinghaoxiang <michael19950415@outlook.com>
2021-06-03 11:48:28 +0800

    add files

---
+++ application/js/square.js
@@ -0,0 +1,76 @@
+class Square {
+	constructor() {
+		this.data = [
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0]
+		];
+		this.origin = {
+			x: 0,
+			y: 0
+		};
+		this.dir = 0;
+	}
+	canRotate(isValid) {
+		var d = (this.dir + 1) % 4;
+		if (d == 4) {
+			d = 0;
+		}
+		var test = [
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0]
+		];
+		for (var i = 0; i < this.data.length; i++) {
+			for (var j = 0; j < this.data[0].length; j++) {
+				test[i][j] = this.rotates[d][i][j];
+			}
+		}
+		return isValid(this.origin, test);
+	}
+	rotate(num) {
+		if (!num)
+			num = 1;
+		this.dir = (this.dir + num) % 4;
+		if (this.dir == 4) {
+			this.dir = 0;
+		}
+		for (var i = 0; i < this.data.length; i++) {
+			for (var j = 0; j < this.data[0].length; j++) {
+				this.data[i][j] = this.rotates[this.dir][i][j];
+			}
+		}
+	}
+	down() {
+		this.origin.x = this.origin.x + 1;
+	}
+	canDown(isValid) {
+		var test = {};
+		test.x = this.origin.x + 1;
+		test.y = this.origin.y;
+		return isValid(test, this.data);
+	}
+	canLeft(isValid) {
+		var test = {};
+		test.x = this.origin.x;
+		test.y = this.origin.y - 1;
+		return isValid(test, this.data);
+	}
+	left() {
+		this.origin.y = this.origin.y - 1;
+	}
+	canRight(isValid) {
+		var test = {};
+		test.x = this.origin.x;
+		test.y = this.origin.y + 1;
+		return isValid(test, this.data);
+	}
+	right() {
+		this.origin.y = this.origin.y + 1;
+	}
+}
+Square.prototype.down = function () {
+	this.origin.x = this.origin.x + 1;
+}
---
+++ application/js/squareFactory.js
@@ -0,0 +1,261 @@
+class Square1 {
+	constructor() {
+		Square.call(this);
+		this.rotates = [
+			[
+				[0, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 2, 0, 0]
+			],
+			[
+				[0, 0, 0, 0],
+				[2, 2, 2, 2],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[0, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 2, 0, 0]
+			],
+			[
+				[0, 0, 0, 0],
+				[2, 2, 2, 2],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			]
+		];
+	}
+}
+Square1.prototype = Square.prototype;
+class Square2 {
+	constructor() {
+		Square.call(this);
+		this.rotates = [
+			[
+				[0, 2, 0, 0],
+				[2, 2, 2, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 0, 0, 0],
+				[2, 2, 0, 0],
+				[2, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 2, 2, 0],
+				[0, 2, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[0, 2, 0, 0],
+				[2, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 0, 0, 0]
+			]
+		];
+	}
+}
+Square2.prototype = Square.prototype;
+class Square3 {
+	constructor() {
+		Square.call(this);
+		this.rotates = [
+			[
+				[2, 2, 2, 0],
+				[0, 0, 2, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[0, 2, 0, 0],
+				[0, 2, 0, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 0, 0, 0],
+				[2, 2, 2, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 2, 0, 0],
+				[2, 0, 0, 0],
+				[2, 0, 0, 0],
+				[0, 0, 0, 0]
+			]
+		];
+	}
+}
+Square3.prototype = Square.prototype;
+class Square4 {
+	constructor() {
+		Square.call(this);
+		this.rotates = [
+			[
+				[2, 2, 2, 0],
+				[2, 0, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[0, 0, 2, 0],
+				[2, 2, 2, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 0, 0, 0],
+				[2, 0, 0, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0]
+			]
+		];
+	}
+}
+Square4.prototype = Square.prototype;
+class Square5 {
+	constructor() {
+		Square.call(this);
+		this.rotates = [
+			[
+				[2, 2, 0, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 2, 0, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 2, 0, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 2, 0, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			]
+		];
+	}
+}
+Square5.prototype = Square.prototype;
+class Square6 {
+	constructor() {
+		Square.call(this);
+		this.rotates = [
+			[
+				[0, 2, 2, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 0, 0, 0],
+				[2, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[0, 2, 2, 0],
+				[2, 2, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 0, 0, 0],
+				[2, 2, 0, 0],
+				[0, 2, 0, 0],
+				[0, 0, 0, 0]
+			]
+		];
+	}
+}
+Square6.prototype = Square.prototype;
+class Square7 {
+	constructor() {
+		Square.call(this);
+		this.rotates = [
+			[
+				[2, 2, 0, 0],
+				[0, 2, 2, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[0, 2, 0, 0],
+				[2, 2, 0, 0],
+				[2, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[2, 2, 0, 0],
+				[0, 2, 2, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			],
+			[
+				[0, 2, 0, 0],
+				[2, 2, 0, 0],
+				[2, 0, 0, 0],
+				[0, 0, 0, 0]
+			]
+		];
+	}
+}
+Square7.prototype = Square.prototype;
+class SquareFactory {
+	constructor() {
+	}
+	make(index, dir) {
+		var s;
+		index = index + 1;
+		switch (index) {
+			case 1:
+				s = new Square1();
+				break;
+			case 2:
+				s = new Square2();
+				break;
+			case 3:
+				s = new Square3();
+				break;
+			case 4:
+				s = new Square4();
+				break;
+			case 5:
+				s = new Square5();
+				break;
+			case 6:
+				s = new Square6();
+				break;
+			case 7:
+				s = new Square7();
+				break;
+			default:
+				break;
+		}
+		s.origin.x = 0;
+		s.origin.y = 3;
+		s.rotate(dir);
+		return s;
+	}
+}
